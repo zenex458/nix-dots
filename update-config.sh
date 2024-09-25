303c4c07287dd86ffc62f0a5b00a3b876e871ec1
Gh\x0st <blobman320@gmail.com>
2024-09-25 18:29:46 +0200

    updated my stuff

--- update-config.sh
+++ update-config.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
-cp -r /home/matt/.config/home-manager/* ./home-manager/;
-sudo cp /home/matt/.config/home-manager/home.nix /etc/nixos/home.nix;
-cp -r /etc/nixos/* .;
-sudo nixos-rebuild switch;
-git add *;
-git commit -m "updated my stuff";
+#!/usr/bin/env bash
+if [[! -e /home/matt/.config/home-manager/home.nix]]; then
+	mkdir /home/matt/.config/home-manager
+	cp -r ./home-manager/* /home/matt/.config/home-manager
+	home-manager switch
+fi
+cp -r /home/matt/.config/home-manager/* ./home-manager/
+sudo cp /home/matt/.config/home-manager/home.nix /etc/nixos/home.nix
+cp -r /etc/nixos/* .
+sudo nixos-rebuild switch
+git add *
+git commit -m "updated my stuff"
 git push
