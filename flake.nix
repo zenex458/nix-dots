595103d211d5b04107b682f010561a42d100a36d
Gh\x0st <blobman320@gmail.com>
2024-09-25 17:31:25 +0200

    updated my stuff

--- flake.nix
+++ flake.nix
@@ -2,11 +2,23 @@
 	description = "Hello";
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
 	};
-	outputs = inputs@{ self, nixpkgs, ...}: {
+	outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
-			modules = [ ./configuration.nix ];
+			modules = [
+				./configuration.nix 
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.matt = import ./home.nix;
+				}
+			];
 		};
 	};
 }
--- update-config.sh
+++ update-config.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-cp -r /etc/nixos/* .;
 cp -r /home/matt/.config/home-manager/* ./home-manager/;
+sudo cp /home/matt/.config/home-manager/home.nix /etc/nixos/home.nix;
+cp -r /etc/nixos/* .;
 git add *;
 git commit -m "updated my stuff";
 git push
-
-
