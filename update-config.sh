0d245af7cad119043a461b6a592569474ee297ae
Gh\x0st <blobman320@gmail.com>
2024-09-25 17:33:06 +0200

    updated my stuff

--- update-config.sh
+++ update-config.sh
@@ -2,6 +2,7 @@
 cp -r /home/matt/.config/home-manager/* ./home-manager/;
 sudo cp /home/matt/.config/home-manager/home.nix /etc/nixos/home.nix;
 cp -r /etc/nixos/* .;
+sudo nixos-rebuild switch;
 git add *;
 git commit -m "updated my stuff";
 git push
