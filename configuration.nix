c80ea463e42d687e81432c1e18b0474992c9f705
Gh\x0st <blobman320@gmail.com>
2024-10-07 18:13:55 +0200

    updated my stuff

--- configuration.nix
+++ configuration.nix
@@ -33,7 +33,7 @@
   console = {
   #   font = "Lat2-Terminus16";
     keyMap = "uk";
-    font = "NotoSans Nerd Font";
+    font = "NotoSans NFP";
   #   useXkbConfig = true; # use xkb.options in tty.
   };
 
