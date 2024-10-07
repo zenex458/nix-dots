fd9776537fe4fee606b0f8a6c2d56b5c6a3dc0c5
Gh\x0st <blobman320@gmail.com>
2024-10-08 01:36:05 +0200

    updated my stuff

--- home.nix
+++ home.nix
@@ -80,12 +80,6 @@
     {
     # will source zsh-autosuggestions.plugin.zsh
     name = "zsh-autosuggestions";
-      src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-autosuggestions";
-        rev = "0.4.0";
-        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-      };
     }
     { name = "zsh-syntax-highlighting"; src = pkgs.zsh-syntax-highlighting; }
     ];
