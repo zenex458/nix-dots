0a5f830f72dd1e604017c34500860531df17fe7a
Gh\x0st <blobman320@gmail.com>
2024-10-08 02:42:31 +0200

    updated my stuff

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
-#!/bin/bash
-echo $1
+#!/usr/bin/env bash
+
+dialogue="Flags: \n-d/--disk = the disk you want these operations on \nhellothere"
+printHelp() {
+  echo -e $dialogue
+}
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -V | --version )
+    echo "$version"
+    exit
+    ;;
+  -f | --flag )
+    printHelp
+    ;;
+  *)
+    printHelp
+  ;;
+esac; shift; done
